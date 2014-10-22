4851ccede1fc59eb59388d550fc2e5cca40645ac
S.P. Mohanty <spmohanty91@gmail.com>
2014-10-23 04:31:36 +0530

    modifying maps

--- js/AdminLTE/dashboard.js
+++ js/AdminLTE/dashboard.js
@@ -49,20 +49,6 @@ $(function() {
     /* jQueryKnob */
     $(".knob").knob();
 
-    //jvectormap data
-    var visitorsData = {
-        "US": 398, //USA
-        "SA": 400, //Saudi Arabia
-        "CA": 1000, //Canada
-        "DE": 500, //Germany
-        "FR": 760, //France
-        "CN": 300, //China
-        "AU": 700, //Australia
-        "BR": 600, //Brazil
-        "IN": 800, //India
-        "GB": 320, //Great Britain
-        "RU": 3000 //Russia
-    };
     //World map by jvectormap
     $('#world-map').vectorMap({
         map: 'world_mill_en',
@@ -75,17 +61,6 @@ $(function() {
                 "stroke-width": 0,
                 "stroke-opacity": 1
             }
-        },
-        series: {
-            regions: [{
-                    values: visitorsData,
-                    scale: ["#92c1dc", "#ebf4f9"],
-                    normalizeFunction: 'polynomial'
-                }]
-        },
-        onRegionLabelShow: function(e, el, code) {
-            if (typeof visitorsData[code] != "undefined")
-                el.html(el.html() + ': ' + visitorsData[code] + ' new visitors');
         }
     });
 
